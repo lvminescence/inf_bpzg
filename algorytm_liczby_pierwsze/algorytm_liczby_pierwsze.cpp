56e3781ea408972391f42914adaef89de8e595ff
Bartłomiej Pawlus <bpawlus372@technikumkreatywne.pl>
2022-03-02 10:09:48 +0100

    o no nie dziala ojoj ojjjjjjjj no nie

--- algorytm_liczby_pierwsze/algorytm_liczby_pierwsze.cpp
+++ algorytm_liczby_pierwsze/algorytm_liczby_pierwsze.cpp
@@ -1,20 +1,32 @@
-﻿// algorytm_liczby_pierwsze.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <iostream>
 
-#include <iostream>
+using namespace std;
+int n;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int liczbypierwsze(int a) {
+	int lp;
+	int p;
+	lp = 0;
+	p = 2;
+	while (lp < a)
+	{
+		for (int d = 2; d < p-1; d++)
+		{
+			if (p % d == 0)
+			{
+				p = p + 1;
+			}
+			else {
+				cout << p << endl;
+				lp = lp + 1;
+			}
+		}
+	}
+	return 0;
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
+int main() {
+	cout << "Podaj ilosc liczb pierwszych ktore chcesz wymienic: \n";
+	cin >> n;
+	liczbypierwsze(n);
+}
