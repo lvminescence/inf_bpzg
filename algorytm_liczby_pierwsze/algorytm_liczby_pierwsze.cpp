42cb456093eeac56fc626a312c14f1e1d7ea7c7a
Bartłomiej Pawlus <bpawlus372@technikumkreatywne.pl>
2022-03-09 09:59:04 +0100

    juz dzialaaa yayyyy :))))))))))) zycie jest takie piekne

--- algorytm_liczby_pierwsze/algorytm_liczby_pierwsze.cpp
+++ algorytm_liczby_pierwsze/algorytm_liczby_pierwsze.cpp
@@ -4,29 +4,26 @@ using namespace std;
 int n;
 
 int liczbypierwsze(int a) {
-	int lp;
-	int p;
-	lp = 0;
-	p = 2;
+	cout << "Liczby pierwsze w tym zakresie to: \n";
+	int lp = 0;
+	int p = 2;
 	while (lp < a)
 	{
-		for (int d = 2; d < p-1; d++)
+		for (int d = 2; d < p - 1; d++)
 		{
 			if (p % d == 0)
 			{
 				p = p + 1;
 			}
-			else {
-				cout << p << endl;
-				lp = lp + 1;
-			}
 		}
-	}
-	return 0;
+		cout << p << endl;
+		lp = lp + 1;
+		p = p + 1;
+	} return 0;
 }
 
 int main() {
 	cout << "Podaj ilosc liczb pierwszych ktore chcesz wymienic: \n";
 	cin >> n;
 	liczbypierwsze(n);
-}
+} 
