4ec0931adb7094781a6568bf50578d24707eac7b
Bartłomiej Pawlus <bpawlus372@technikumkreatywne.pl>
2022-03-16 10:08:45 +0100

    dodano nww i nwd (wip)

---
+++ algorytm_nww_nwd/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int la;
+int lb;
+
+int nwd(int a, int b) {
+	while (a != b) {
+		if (a < b == true)
+		{
+			b = b - a;
+		}
+		else {
+			a = a - b;
+		}
+	}
+	cout << a;
+	return 0;
+} 
+
+int nww(int a, int b) {
+	int iloczyn;
+	int nww;
+	iloczyn = a * b;
+	while (a != b) {
+		if (a < b == true)
+		{
+			b = b - a;
+		}
+		else {
+			a = a - b;
+		}
+		nww = iloczyn / a;
+		cout << nww;
+		return 0;
+	}
+}
+int main() {
+	cin >> la;
+	cout << endl;
+	cin >> lb;
+	cout << endl;
+	nww(la, lb);
+	return 0;
+}
