6dd5966bbc3bb35585c4c79b870eca9688be2b43
Bartłomiej Pawlus <bpawlus372@technikumkreatywne.pl>
2022-03-30 10:08:53 +0200

    nie dziala ojc

---
+++ algorytm_szyfr_cezara/ConsoleApplication1/szyfrcezara.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+int klucz;
+char tab[1000] = { 'a', 'b', 'c' };
+
+int cezar(int klucza, char tab[1000]) {
+	int dl;
+	bool conti = true;
+	dl = strlen(tab[0]);
+	if (!(klucza >= -26 and klucza <= 26))
+	{
+		conti = false;
+	}
+	if (conti = true)
+	{
+		if (klucz >= 0)
+		{
+			for (int i = 0; i < dl; i++)
+			{
+				if (tab + klucz <= i)
+				{
+					tab = tab + klucz;
+				}
+				else {
+					tab = tab + klucz - 26;
+				}
+			}
+		}
+		else {
+			for (int i = 0; i < dl; i++)
+			{
+				if (tab + klucz >= i)
+				{
+					tab += klucz;
+				}
+				else {
+					tab = tab + klucz + 26;
+				}
+			}
+
+		}
+	}
+}
