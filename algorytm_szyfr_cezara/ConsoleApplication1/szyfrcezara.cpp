34bc1bf37ba47d6069dc518fc04ffa8f5ebfcc0a
Bartłomiej Pawlus <bpawlus372@technikumkreatywne.pl>
2022-04-06 10:10:54 +0200

    at least there's progress

--- algorytm_szyfr_cezara/ConsoleApplication1/szyfrcezara.cpp
+++ algorytm_szyfr_cezara/ConsoleApplication1/szyfrcezara.cpp
@@ -1,43 +1,47 @@
 ﻿#include <iostream>
+#include <cstring>
 using namespace std;
-int klucz;
-char tab[1000] = { 'a', 'b', 'c' };
+char tablica[2000];
 
-int cezar(int klucza, char tab[1000]) {
+int cezar(int klucz, char tab[2000]) {
 	int dl;
+	dl = sizeof(tab);
 	bool conti = true;
-	dl = strlen(tab[0]);
-	if (!(klucza >= -26 and klucza <= 26))
+	if (not (klucz >= -26 && klucz <= 26))
 	{
 		conti = false;
 	}
-	if (conti = true)
+	if (klucz >= 0)
 	{
-		if (klucz >= 0)
+		for (int i = 0; i < dl; i++)
 		{
-			for (int i = 0; i < dl; i++)
-			{
-				if (tab + klucz <= i)
-				{
-					tab = tab + klucz;
-				}
-				else {
-					tab = tab + klucz - 26;
-				}
+			if (tab[i] + klucz <= 'z') {
+				tab = tab + klucz;
 			}
-		}
-		else {
-			for (int i = 0; i < dl; i++)
-			{
-				if (tab + klucz >= i)
-				{
-					tab += klucz;
-				}
-				else {
-					tab = tab + klucz + 26;
-				}
+			else {
+				tab = tab + klucz - 26;
 			}
-
 		}
 	}
+	else {
+		for (int i = 0; i < dl; i++) {
+			if (tab[i] + klucz <= 'a') {
+				tab = tab + klucz;
+			}
+			else {
+				tab = tab + klucz + 26;
+			}
+		}
+	}
+	cout << tab[0];
+	return 0;
 }
+
+int main() {
+	int a;
+	cin >> tablica;
+	cout << "placeholder";
+	cin >> a;
+	cezar(a, tablica);
+}
+
