e42661521d75d7cdac9610e4c61e8ddb9ccfb2cd
Bartłomiej Pawlus <bpawlus372@technikumkreatywne.pl>
2022-04-27 09:46:12 +0200

    poprawki w algorytmie wyszukiwania binarnego

--- algorytm_wyszukiwanie_binarne/algorytm_wyszukiwanie_binarne.cpp
+++ algorytm_wyszukiwanie_binarne/algorytm_wyszukiwanie_binarne.cpp
@@ -2,18 +2,20 @@
 
 
 int licz;
+int tab[15] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 };
 using namespace std;
 
 int szukaj(int szukana) {
-    int tab[15] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 };
     int l, p, sr;
+    bool found = false;
     l = 0;
     p = 15;
     sr = (l + p) / 2;
     while (l <= p)
     {
-        if (tab[sr] = szukana) {
+        if (tab[sr] == szukana) {
             cout << "Liczba " << szukana << " znajduje sie w komorce o indeksie " << sr << ".\n";
+            found = true;
         }
         if (tab[sr] > szukana) {
             p = sr - 1;
@@ -22,12 +24,16 @@ int szukaj(int szukana) {
         }
         sr = (l + p) / 2;
     }
+    if (found == false) {
+        cout << "Liczba " << szukana << " nie znajduje sie w zadnym indeksie.";
+    }
     return 0;
 }
 
 int main()
 {
-    cout << "Podaj liczbe, ktora chcesz znalezc";
+    cout << "   [ Wyszukiwanie naturalne ]\n/Bartlomiej Pawlus, Zofia Grabczewska/\n\n\n";
+    cout << "Podaj liczbe, ktora chcesz znalezc! \n";
     cin >> licz;
     szukaj(licz);
 }
